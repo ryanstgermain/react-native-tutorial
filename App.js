8f74cafc6b6a6d64b6d199bb16b5fc2bec8f7867
ryanstgermain <ryanjoel26@gmail.com>
2020-01-29 15:25:32 -0700

    Made it so you can type in an input and it adds it to a list.

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [ enteredGoal, setEnteredGoal ] = useState('');
+  const [ courseGoals, setCourseGoals ] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [ ...courseGoals, enteredGoal ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder='Course Goal'
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button
+          title='ADD'
+          onPress={addGoalHandler}
+        />
+      </View>
+      <View>
+        {courseGoals.map((goal) =>
+          <Text key={goal}>{goal}</Text>
+        )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  input: {
+    width: '80%',
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 10
+  }
 });
